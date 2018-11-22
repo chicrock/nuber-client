d31c67d63bf21e6192ba939cba5dcc3d3aeff3eb
chicrock <jungsungrak@gmail.com>
2018-11-22 18:27:04 +0900

    Setup default ts style

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,5 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
