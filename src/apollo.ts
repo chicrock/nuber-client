1905521349e3d99598aeb73e683a453dd2233f74
chicrock <jungsungrak@gmail.com>
2018-11-22 18:50:01 +0900

    Add resolvers  and clientState for reqeust authorization

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,6 +1,46 @@
-import ApolloClient from "apollo-boost";
+import ApolloClient, { Operation } from "apollo-boost";
 
 const client = new ApolloClient({
+  clientState: {
+    defaults: {
+      auth: {
+        __typename: "Auth",
+        isLoggedIn: Boolean(localStorage.getItem("jwt")),
+      },
+    },
+    resolvers: {
+      Mutaion: {
+        logUserIn: (_, { token }, { cache }) => {
+          localStorage.setItem("jwt", token);
+          cache.writeData({
+            data: {
+              auth: {
+                __typename: "Auth",
+                isLoggedIn: true,
+              },
+            },
+          });
+
+          return null;
+        },
+        logUserOut: (_, __, { cache }) => {
+          localStorage.removeItem("jwt");
+          cache.writeData({
+            data: {
+              __typename: "Auth",
+              isLoggedIn: false,
+            },
+          });
+          return null;
+        },
+      },
+    },
+  },
+  request: async (operation: Operation) => {
+    operation.setContext({
+      headers: { "X-JWT": localStorage.getItem("jwt") || "" },
+    });
+  },
   uri: "http://localhost:4000/graphql",
 });
 
